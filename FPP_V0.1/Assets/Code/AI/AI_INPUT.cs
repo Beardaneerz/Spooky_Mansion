129d8f41358ebb3fe72445b6065ca4979b2aa38e
Arnaud <arnaud@kujirastudio.com>
2016-04-12 13:17:30 +0100

    inputManager_V1.01

--- FPP_V0.1/Assets/Code/AI/AI_INPUT.cs
+++ FPP_V0.1/Assets/Code/AI/AI_INPUT.cs
@@ -10,23 +10,26 @@ using System.Collections;
 public class AI_INPUT : MonoBehaviour {
 
 	public int nID = 0;
+	public float nSpeed = 0.1F;
 
+	private Rigidbody hObjectRigidbody;
 	private float nXLeft = 0.0F;
 	private float nYLeft = 0.0F;
 
 	void Start () {
-	
+
+		hObjectRigidbody = GetComponent("Rigidbody") as Rigidbody;
 	}
 
 	void Move () {
 
-		this.gameObject.transform.position += new Vector3 (nXLeft, nYLeft, 0);
+		hObjectRigidbody.AddForce (new Vector3 (nXLeft, nYLeft, 0)*nSpeed);
 	}
 
 	void Update () {
 	
 		if (Input.GetAxis ("AXIS_X_1_" + nID) != 0) {
-			Debug.Log ("player : " + nID + " AxisX : " + Input.GetAxis ("AXIS_X_1_" + nID));
+			// Debug.Log ("player : " + nID + " AxisX : " + Input.GetAxis ("AXIS_X_1_" + nID));
 			nXLeft = Input.GetAxis ("AXIS_X_1_" + nID);
 		} 
 		else {
@@ -34,7 +37,7 @@ public class AI_INPUT : MonoBehaviour {
 		}
 
 		if (Input.GetAxis ("AXIS_Y_1_"+nID)!= 0){
-			Debug.Log ("player : "+ nID + " AxisY : " + Input.GetAxis ("AXIS_Y_1_"+nID));
+			// Debug.Log ("player : "+ nID + " AxisY : " + Input.GetAxis ("AXIS_Y_1_"+nID)); 
 			nYLeft = Input.GetAxis ("AXIS_Y_1_"+nID);
 		}
 		else {
